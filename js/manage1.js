a3af4996fc2ebe63d0556ea3f0bd6c252a89b0cc
Z-Xuan Hong <gay88358@yahoo.com.tw>
2017-06-27 08:19:13 +0800

    final

--- js/manage1.js
+++ js/manage1.js
@@ -287,45 +287,4 @@ $(document).ready(function() {
           modal.style.display = "none";
       }
 
-      $('#download').click(function() {
-          var storage = firebase.storage();
-          var pathReference = storage.ref('images/6684584839_51db08923d_b.jpg');
-          // Create a reference to the file we want to download
-          // Get the download URL
-          pathReference.getDownloadURL().then(function(url) {
-            // Insert url into an <img> tag to "download"
-              console.log('download url: ' + url);
-              var xhr = new XMLHttpRequest();
-              xhr.responseType = 'blob';
-              xhr.onload = function(event) {
-                var blob = xhr.response;
-              };
-              xhr.open('GET', url);
-              xhr.send();
-
-          }).catch(function(error) {
-
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
-            switch (error.code) {
-              case 'storage/object_not_found':
-                // File doesn't exist
-                break;
-
-              case 'storage/unauthorized':
-                // User doesn't have permission to access the object
-                break;
-
-              case 'storage/canceled':
-                // User canceled the upload
-                break;
-              case 'storage/unknown':
-                // Unknown error occurred, inspect the server response
-                break;
-            }
-          });
-      });
-
-
-
 });
